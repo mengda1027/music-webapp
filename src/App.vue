d1a37c2d3637a9ca79a37b0fc7ff473c08b7e911
mengda1027 <mengda1027@qq.com>
2017-07-24 00:58:59 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    music webapp based on Vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable.styl"
+
+  #app
+    color: $color-theme
+</style>
